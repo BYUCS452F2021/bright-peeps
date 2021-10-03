371f3d34fc611fb2d3b49b06be49b317bdaf9945
Rogerio Cruz <rogvc@outlook.com>
2021-10-03 10:57:14 -0600

    Initial Vue.js/Nuxt.js set up

---
+++ src/BrightPeeps.Web/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-gray-900 p-4">
+    <h1 class="text-3xl font-bold">Welcome to Bright Peeps</h1>
+    <p class="mt-4 text-lg">
+      Pretty soon this website will look pretty awesome and do some great
+      things.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
