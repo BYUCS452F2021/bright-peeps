21675e32b013c6b34ec1b83118c69a260dd14db8
johnny123214133 <54080177+johnny123214133@users.noreply.github.com>
2021-10-06 02:00:05 -0600

    started adding peep stuff...

---
+++ src/BrightPeeps.Api/Queries/Peeps/GetAllPeeps.cs
@@ -0,0 +1,56 @@
+using System.Threading;
+using System.Threading.Tasks;
+using BrightPeeps.Api.Utils;
+using BrightPeeps.Core.Models;
+using BrightPeeps.Core.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BrightPeeps.Api.Queries.Peeps
+{
+    public static class GetAllPeeps
+    {
+        public sealed class Request : IRequest<QueryResponse> { }
+
+        public class Handler : IRequestHandler<Request, QueryResponse>
+        {
+            private readonly ISqlDataAccessService Data;
+            private readonly ILogger<Handler> Logger;
+
+            public Handler(ISqlDataAccessService dataAccess, ILogger<Handler> logger)
+            {
+                Data = dataAccess;
+                Logger = logger;
+            }
+
+            public async Task<QueryResponse> Handle(Request request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var results = await Data.ExecuteStoredProcedure<PersonProfile, dynamic>(
+                        procedureId: "getAllPeeps",
+                        parameters: null
+                    );
+
+                    return new QueryResponse
+                    {
+                        Successful = true,
+                        Message = "Data retrieved successfully.",
+                        Result = results
+                    };
+                }
+                catch (System.Exception e)
+                {
+                    Logger?.LogError($"Could not retrieve data from database. Error was {e}");
+
+                    return new QueryResponse
+                    {
+                        Successful = false,
+                        Message = "Could not retrieve data from database. Check logs for more details.",
+                        Result = default
+                    };
+                }
+            }
+        }
+    }
+}
