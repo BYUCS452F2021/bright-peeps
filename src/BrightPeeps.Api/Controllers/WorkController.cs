fe3cba1a3807b9b8944e1607cb65894f44f09a35
bradybess <bess.brady9@gmail.com>
2021-10-11 16:35:31 -0600

    Add Works Controller and Queries

---
+++ src/BrightPeeps.Api/Controllers/WorkController.cs
@@ -0,0 +1,133 @@
+using System.Threading.Tasks;
+using BrightPeeps.Api.Commands.Persons;
+using BrightPeeps.Api.Queries;
+using BrightPeeps.Api.Queries.Persons;
+using BrightPeeps.Api.Queries.Works;
+using BrightPeeps.Core.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrightPeeps.Api.Controllers 
+{
+    [Route("work")]
+    public partial class WorkController: Controller
+    {
+        private readonly IMediator Mediator;
+
+        public WorkController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        //GET -
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWorks()
+        {
+            var result = await Mediator.Send(new GetAllWorks.Request());
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        [HttpGet("{type}")]
+        public async Task<IActionResult> GetWorksByType(string type)
+        {
+            var result = await Mediator.Send(new GetWorksByType.Request(type));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWorkById(int id) {
+            var result = await Mediator.Send(new GetWorkById.Request(id));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        [HttpGet("{peepId}")]
+        public async Task<IActionResult> GetWorksByPeepId(int peepId) {
+            var result = await Mediator.Send(new GetWorksByPeepId.Request(peepId));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        //POST -
+
+        [HttpPost]
+        public async Task<IActionResult> InsertWork(int peepId, string workType, string workDesc, string workUrl, string workTitle) {
+            var result = await Mediator.Send(new InsertWork.Request(peepId, workType, workDesc, workUrl, workTitle));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        //PUT -
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateWork(int id, int peepId, string workType, string workDesc, string workUrl, string workTitle) {
+            var result = await Mediator.Send(new UpdateWork.Request(id, peepId, workType, workDesc, workUrl, workTitle));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        //DELETE - RemoveWork(int id)
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveWorkById(int id) {
+            var result = await Mediator.Send(new RemoveWorkById.Request(id));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        [HttpDelete("{peepId}")]
+        public async Task<IActionResult> RemoveWorkByPeepId(int peepId) {
+            var result = await Mediator.Send(new RemoveWorksByPeepId.Request(peepId));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+
+        [HttpDelete("{type}")]
+        public async Task<IActionResult> RemoveWorkByType(string type) {
+            var result = await Mediator.Send(new RemoveWorksByType.Request(type));
+
+            if (result.Successful) {
+                return Ok(result);
+            } else {
+                return NotFound(result);
+            }
+        }
+    }
+
+
+}
---
+++ src/BrightPeeps.Api/Queries/Works/UpdateWork.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BrightPeeps.Api.Utils;
+using BrightPeeps.Core.Models;
+using BrightPeeps.Core.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BrightPeeps.Api.Queries.Works {
+    public static class UpdateWork {
+        public sealed class Request : IRequest<QueryResponse> {
+            public int Id;
+            public int PeepId;
+            public string WorkType;
+            public string WorkDesc;
+            public string WorkUrl;
+            public string WorkTitle;
+
+            public Request(int id, int peepId, string type, string desc, string url, string title) {
+                Id = id;
+                PeepId = peepId;
+                WorkType = type;
+                WorkDesc = desc;
+                WorkUrl = url;
+                WorkTitle = title;
+            }
+        }
+
+        public class Handler : IRequestHandler<Request, QueryResponse> {
+            private readonly ISqlDataAccessService Data;
+            private readonly ILogger<Handler> Logger;
+
+            public Handler(ISqlDataAccessService dataAccess, ILogger<Handler> logger) {
+                Data = dataAccess;
+                Logger = logger;
+            }
+
+            public async Task<QueryResponse> Handle(Request request, CancellationToken cancellationToken) {
+                try {
+                    var results = await Data.ExecuteStoredProcedure<PersonProfile, dynamic>(
+                        procedureId: "UpdateWork",
+                        parameters: (request.Id, request.PeepId, request.WorkType, request.WorkDesc, request.WorkUrl, request.WorkTitle)
+                    );
+
+                    return new QueryResponse {
+                        Successful = true,
+                        Message = "Data retrieved successfully.",
+                        Result = results
+                    };
+                } catch (System.Exception e) {
+                    Logger?.LogError($"Could not retrieve data from database. Error was {e}");
+
+                    return new QueryResponse {
+                        Successful = false,
+                        Message = "Could not retrieve data from database. Check logs for more details.",
+                        Result = default
+                    };
+                }
+            }
+        }
+    }
+}
+
